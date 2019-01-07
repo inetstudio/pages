e6e7067ff46cf9c8b571ab5fc916597bd67b291c
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-08 01:55:40 +0300

    Update icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.pages.*') }}">
-    <a href="{{ route('back.pages.index') }}"><i class="fa fa-file-o"></i> <span class="nav-label">Страницы</span></a>
+    <a href="{{ route('back.pages.index') }}"><i class="fa fa-file"></i> <span class="nav-label">Страницы</span></a>
 </li>
