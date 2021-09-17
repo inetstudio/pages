913bcac293919b4f14054accf696fcad44d56496
Konstantin Bukin <bukin@inetstudio.ru>
2021-09-17 23:52:08 +0300

    Update menu

--- entities/pages/resources/views/back/includes/navigation.blade.php
+++ entities/pages/resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.pages.*') }}">
+<li class="{{ isActiveRoute('back.pages.*', 'mm-active') }}">
     <a href="{{ route('back.pages.index') }}"><i class="fa fa-file"></i> <span class="nav-label">Страницы</span></a>
 </li>
